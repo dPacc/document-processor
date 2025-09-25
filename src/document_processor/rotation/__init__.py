67fbe1db4873d4ecda0674a65f78278a99fadf01
dPacc <themdk01@gmail.com>
2025-09-25 12:09:41 +0400

    add: rotation module with angle detection and correction algorithms

---
+++ src/document_processor/rotation/__init__.py
@@ -0,0 +1,8 @@
+"""
+Rotation detection and correction utilities.
+"""
+
+from .detector import RotationDetector
+from .corrector import RotationCorrector
+
+__all__ = ["RotationDetector", "RotationCorrector"]
